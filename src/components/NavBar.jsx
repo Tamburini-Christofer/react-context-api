32ee4eab6bbbafddb2005fe62c3c104cc6103095
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-10-08 14:24:38 +0200

    Creazione del Bottone Budget e applicazione

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,33 @@
 import { NavLink } from "react-router-dom";
+import { useBudget } from "../components/Budget";
 
 const linkPage = [
-    { route: '/', label: 'HomePage' },
-    { route: '/Chi siamo', label: 'Chi siamo' },
-    { route: '/Prodotti', label: 'Prodotti' }
-]
+  { route: "/", label: "HomePage" },
+  { route: "/Chi siamo", label: "Chi siamo" },
+  { route: "/Prodotti", label: "Prodotti" },
+];
 const NavBar = () => {
-    return (
-        <>
-        <nav>
-            <ul>
-                {linkPage.map(link => (
-                    <li>
-                        <NavLink to={link.route}>{link.label}</NavLink>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-        </>
-    )
-}
+  const { budget, setBudget } = useBudget();
+  return (
+    <>
+      <nav>
+        <ul>
+          {linkPage.map((link) => (
+            <li>
+              <NavLink to={link.route}>{link.label}</NavLink>
+            </li>
+          ))} 
+          <button
+          className="back-btn budget"
+          onClick={() => setBudget((prev) => !prev)}>
+          {budget ? "Disattiva Modalità Budget" : "Attiva Modalità Budget"}
+        </button>
+        </ul>
+        
+       
+      </nav>
+    </>
+  );
+};
 
 export default NavBar;
